be630cc9b370d9e8469e8990f38018905733bae6
Rahul Sharma <rsconceptx@gmail.com>
2015-10-08 18:45:15 +0530

    Update last_element.hs

--- 99Problems/last_element.hs
+++ 99Problems/last_element.hs
@@ -1,5 +1,9 @@
 -- Getting the last element from a list of n numbers using recursion
 
-my_last n = if length n == 1 then head [] else my_last (tail n)
+my_last n = if length n == 0
+            then (error "Empty list has no end defined")
+            else if length n == 1 
+            then head [] 
+            else my_last (tail n)
 
 main = print (my_last [1,2,3,4])
